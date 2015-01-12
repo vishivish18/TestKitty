39f5e7f7e7d471d866b6141c6efa05ec632ac921
unknown <Vishal.Ranjan@VRANJANT420.tf-net.tribalfusion.com>
2015-01-12 16:36:53 +0530

    added app.js in assets.js to listen to emit emitted from server to client

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * app.js
+ *
+ * Front-end code and event handling for sailsChat
+ *
+ */
+
+
+// Attach a listener which fires when a connection is established:
+io.socket.on('connect', function socketConnected() {
+
+    // Show the main UI
+  console.log('this is emitted from client');
+
+});
